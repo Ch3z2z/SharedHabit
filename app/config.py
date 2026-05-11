3157f42e1bf040e832d9ac84b289fa8749d711ce
Egor Chernobai <Chernobai.E.S@yandex.ru>
2026-05-11 18:35:45 +0300

    done

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/SharedHabbit"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/SharedHabit"
     JWT_SECRET_KEY = "super-secret"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
