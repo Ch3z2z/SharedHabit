58d9dabad527a71200f2ce2241e9f802bd33bbae
Egor Chernobai <Chernobai.E.S@yandex.ru>
2026-05-13 11:23:17 +0300

    add docker

--- app/config.py
+++ app/config.py
@@ -1,4 +1,9 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/SharedHabit1"
-    JWT_SECRET_KEY = "super-secret"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:postgres@localhost/SharedHabit"
+    )
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
